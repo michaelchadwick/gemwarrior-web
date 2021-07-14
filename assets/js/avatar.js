c6ee5c80ef8271dfcd9a977d846dbe74c6211e46
Michael Chadwick <mchadwick@ucsd.edu>
2021-07-14 11:45:46 -0700

    moved avatar files into subdir

--- assets/js/avatar.js
+++ assets/js/avatar.js
@@ -3,31 +3,31 @@ onmessage = function(msg) {
 
   switch (msg.data) {
     case 'standing':
-      xhr.open('GET', '../data/player-standing.txt')
+      xhr.open('GET', '../data/avatar/player-standing.txt')
       xhr.send()
       break
     case 'standing-blink':
-      xhr.open('GET', '../data/player-standing-blink.txt')
+      xhr.open('GET', '../data/avatar/player-standing-blink.txt')
       xhr.send()
       break
     case 'sitting':
-      xhr.open('GET', '../data/player-sitting.txt')
+      xhr.open('GET', '../data/avatar/player-sitting.txt')
       xhr.send()
       break
     case 'sitting-blink':
-      xhr.open('GET', '../data/player-sitting-blink.txt')
+      xhr.open('GET', '../data/avatar/player-sitting-blink.txt')
       xhr.send()
       break
     case 'reclining1':
-      xhr.open('GET', '../data/player-reclining1.txt')
+      xhr.open('GET', '../data/avatar/player-reclining1.txt')
       xhr.send()
       break
     case 'reclining2':
-      xhr.open('GET', '../data/player-reclining2.txt')
+      xhr.open('GET', '../data/avatar/player-reclining2.txt')
       xhr.send()
       break
     case 'reclining3':
-      xhr.open('GET', '../data/player-reclining3.txt')
+      xhr.open('GET', '../data/avatar/player-reclining3.txt')
       xhr.send()
       break
   }
