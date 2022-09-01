fe89724e0426c5c6c43012cc5cdfcabd730dabdc
Michael Chadwick <mike@neb.host>
2022-09-01 13:34:24 -0700

    linting and refactoring

--- assets/js/app/constants.js
+++ assets/js/app/constants.js
@@ -1,5 +1,6 @@
 /* constants */
 /* set any global constants */
+/* eslint-disable no-unused-vars */
 
 const GW_SETTINGS_KEY = 'gemwarrior-settings'
 const GW_STATE_KEY = 'gemwarrior-state'
@@ -40,34 +41,31 @@ const GW_COMMANDS = {
     'stopbgm',
   ]
 }
-const GW_LOCATION = {
-  'title': 'Inescapable Hole of Turbidity',
-  'objects': ['rock']
-}
 
 const GW_DEFAULTS = {
-  "config": {
-    "player": {
-      "level": 1,
-      "xp": 0,
-      "hp": 10,
-      "rox": 2,
-      "status": 'standing',
-      "inventory": [
+  'config': {
+    'player': {
+      'level': 1,
+      'xp': 0,
+      'hp': 10,
+      'rox': 2,
+      'status': 'standing',
+      'inventory': [
         'broken flashlight',
         'candlestick holder'
       ]
     },
-    "avatarWorker": null,
-    "blinker": null,
-    "history": [],
-    "historyMarker": 0,
-    "keyCommand": '',
-    "soundInterval": null,
-    "synth": null,
-    "text": ''
+    'avatarWorker': null,
+    'blinker': null,
+    'history': [],
+    'historyMarker': 0,
+    'keyCommand': '',
+    'soundInterval': null,
+    'synth_bgm': null,
+    'synth_fx': null,
+    'text': ''
   },
-  "settings": {
+  'settings': {
     'textSize': '16',
     'playSound': false,
     'showAvatar': false
