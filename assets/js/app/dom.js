af37e69db71abdc7e9fdb7f8283e95e625ebe810
Michael Chadwick <mike@neb.host>
2022-09-01 10:16:07 -0700

    linting

--- assets/js/app/dom.js
+++ assets/js/app/dom.js
@@ -4,26 +4,26 @@
 
 // DOM > main divs/elements
 GemWarrior.dom = {
-  "navOverlay": $('#nav-overlay'),
-  "navContent": ('$nav-content'),
-  "output": $('#output'),
-  "interface": $('#interface'),
-  "stats": $('#interface #stats'),
-  "statsLV": $('#interface #stats #level'),
-  "statsXP": $('#interface #stats #xp'),
-  "statsROX": $('#interface #stats #rox'),
-  "statsHP": $('#interface #stats #hp'),
-  "statsLOC": $('#interface #location #loc'),
-  "avatar": $('#interface #avatar')
+  'navOverlay': $('#nav-overlay'),
+  'navContent': ('$nav-content'),
+  'output': $('#output'),
+  'interface': $('#interface'),
+  'stats': $('#interface #stats'),
+  'statsLV': $('#interface #stats #level'),
+  'statsXP': $('#interface #stats #xp'),
+  'statsROX': $('#interface #stats #rox'),
+  'statsHP': $('#interface #stats #hp'),
+  'statsLOC': document.querySelector('#interface #location #loc'),
+  'avatar': $('#interface #avatar')
 }
 
 // DOM > interactive elements
 GemWarrior.dom.interactive = {
-  "btnNav": $('#button-nav'),
-  "btnNavClose": $('#button-nav-close'),
-  "btnHelp": $('#button-help'),
-  "btnSettings": $('#button-settings'),
-  "cmdInput": $('#command-input'),
-  "keyboard": $('#keyboard'),
-  "keyboardInput": $('#keyboard-input')
+  'btnNav': $('#button-nav'),
+  'btnNavClose': $('#button-nav-close'),
+  'btnHelp': $('#button-help'),
+  'btnSettings': $('#button-settings'),
+  'cmdInput': $('#command-input'),
+  'keyboard': $('#keyboard'),
+  'keyboardInput': $('#keyboard-input')
 }
