9a0c1f1f362aefc3dfde7ecada77c6f885433f1c
Michael Chadwick <mike@neb.host>
2023-07-05 08:00:38 -0700

    moved visual animation styles to own css file; added new shake animation

--- assets/js/app/lib/misc/animation.js
+++ assets/js/app/lib/misc/animation.js
@@ -4,11 +4,12 @@
 
 GemWarrior.animate = (type, q) => {
   if (GemWarrior.settings.enableAnimation) {
+    let speed = 50
+    let intervalId = null
+
     switch (type) {
       case 'flash':
-        const speed = 50
-
-        let intervalId = setInterval(
+        intervalId = setInterval(
           () => document.querySelector('body').classList.toggle('animate-body-flash'),
           speed
         )
@@ -19,6 +20,18 @@ GemWarrior.animate = (type, q) => {
             document.querySelector('body').classList.remove('animate-body-flash')
           },
           speed * q * 2
+        )
+
+        break
+
+      case 'shake':
+        document.querySelector('#output').classList.toggle('animate-output-move-shake')
+
+        setTimeout(
+          () => {
+            document.querySelector('#output').classList.remove('animate-output-move-shake')
+          },
+          q * 1000
         )
 
         break
